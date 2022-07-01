dc275c17213115ac425e7a43573db1dd004c9aac
SaparowMaksat <102533562+SaparowMaksat@users.noreply.github.com>
2022-07-01 04:34:23 +0300

    Upgrade Router

--- src/App/app.js
+++ src/App/app.js
@@ -1,6 +1,19 @@
 import React from "react";
-import Users from "./components/users";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import NavBar from "./components/navBar";
 
 export default function App() {
-    return <Users />;
+    return <div>
+        <NavBar />
+        <Switch>
+            <Route path="/users/:userId?" component={Users} />
+            <Route path="/login" component={Login} />
+            <Route path="/" exact component={Main} />
+            <Redirect to="/" />
+        </Switch>
+    </div>;
 }
--- src/App/components/usersList.jsx
+++ src/App/components/usersList.jsx
@@ -7,7 +7,7 @@ import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
 import UserTable from "./usersTable";
 import _ from "lodash";
-const Users = () => {
+const UsersList = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfession] = useState();
     const [selectedProf, setSelectedProf] = useState();
@@ -114,8 +114,8 @@ const Users = () => {
     return "loading...";
 };
 
-Users.propTypes = {
+UsersList.propTypes = {
     users: PropTypes.array
 };
 
-export default Users;
+export default UsersList;
